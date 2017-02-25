a3aba83ff82298c8863e81f0350c14410bae5195
Philip Dai <smartielion@gmail.com>
2017-02-25 02:07:28 -0500

    Add hexValue input form and hex value validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormBuilder, FormGroup, Validators, AbstractControl} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  hexForm: FormGroup;
+  hexValue: AbstractControl;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  buildForm(): void {
+    this.hexForm = this.fb.group({
+      'hexValue': ['', [
+        Validators.pattern(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/)
+      ]]
+    });
+
+    this.hexValue = this.hexForm.controls['hexValue'];
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    ReactiveFormsModule,
+    HttpModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
