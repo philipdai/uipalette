2387fa3fd9d43b064779671abf73016fd846a0f0
Philip Dai <smartielion@gmail.com>
2017-02-26 18:38:58 -0500

    After page loading hevVal will be set focus automatically

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { SetFocusDirective } from '../directives/set-focus';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SetFocusDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/directives/set-focus.ts
@@ -0,0 +1,16 @@
+import {Directive, AfterViewInit, ElementRef, Renderer} from '@angular/core';
+
+@Directive({
+  selector: '[setFocus]'
+})
+export class SetFocusDirective implements AfterViewInit {
+
+  constructor(public renderer: Renderer, public elementRef: ElementRef) {
+  }
+
+  ngAfterViewInit() {
+    this.renderer.invokeElementMethod(
+      this.elementRef.nativeElement, 'focus', []);
+  }
+
+}
