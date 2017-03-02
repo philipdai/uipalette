ecb2465f865bbfa95b9d06bd59aae9929f784741
Philip Dai <smartielion@gmail.com>
2017-03-01 22:57:16 -0500

    Reduce modal size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -187,7 +187,7 @@ export class AppComponent implements OnInit {
 
   openCopyModal() {
     let config = new MdDialogConfig();
-    config.width = `${this.bodyWidth * 0.5}px`;
+    config.width = `${this.bodyWidth * 0.4}px`;
     config.height = `${this.bodyWidth * 0.32}px`;
     config.disableClose = true;
     let modalRef: MdDialogRef<PhotoModalComponent> = this.modal.open(PhotoModalComponent, config);
