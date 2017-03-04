88970acc69f477f11f943bf684221549978ac94f
Philip Dai <smartielion@gmail.com>
2017-03-03 20:52:52 -0500

    Adjust modal size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -187,8 +187,8 @@ export class AppComponent implements OnInit {
 
   openCopyModal() {
     let config = new MdDialogConfig();
-    config.width = `${this.bodyWidth * 0.4}px`;
-    config.height = `${this.bodyWidth * 0.32}px`;
+    config.width = `500px`;
+    config.height = `400px`;
     config.disableClose = true;
     let modalRef: MdDialogRef<PhotoModalComponent> = this.modal.open(PhotoModalComponent, config);
     modalRef.componentInstance.hslArr1 = this.hslArr1;
